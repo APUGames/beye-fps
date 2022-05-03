9250606d79a06d9196adc818af9130bc6511dda4
eric <ebey16@apu.edu>
2022-05-03 08:59:59 -0700

    final

--- beye-fps/Assets/Scripts/SceneLoader.cs
+++ beye-fps/Assets/Scripts/SceneLoader.cs
@@ -8,7 +8,7 @@ public class SceneLoader : MonoBehaviour
     public void ReloadGame()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void QuitGame()
     {
